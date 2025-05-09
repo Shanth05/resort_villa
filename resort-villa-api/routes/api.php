69d7022fea621547bd177640fcdb2757e7718359
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-09 22:03:39 +0530

    updated

--- resort-villa-api/routes/api.php
+++ resort-villa-api/routes/api.php
@@ -1,26 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ResortController;
-use App\Http\Controllers\VillaController;
-use App\Http\Controllers\RoomController;
-use App\Http\Controllers\BookingController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-// Public routes
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::get('/resorts', [ResortController::class, 'index']);
-Route::get('/resorts/{id}', [ResortController::class, 'show']);
-
-// Protected routes
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::get('/villas/{resort_id}', [VillaController::class, 'getByResort']);
-    Route::get('/rooms/{villa_id}', [RoomController::class, 'getByVilla']);
-
-    Route::post('/bookings', [BookingController::class, 'store']);
-    Route::get('/bookings', [BookingController::class, 'index']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
