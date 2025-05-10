8fa413759907371f537cf027a7e830333c3ef628
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-10 22:40:09 +0530

    Update Booking.php

--- resort-villa-api/app/Models/Booking.php
+++ resort-villa-api/app/Models/Booking.php
@@ -1,20 +1,22 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    protected $fillable = ['user_id', 'room_id', 'check_in', 'check_out', 'status'];
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'villa_id', 'check_in', 'check_out', 'total_price', 'status'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function room()
+    public function villa()
     {
-        return $this->belongsTo(Room::class);
+        return $this->belongsTo(Villa::class);
     }
 }
